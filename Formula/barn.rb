f6da55109feebf3eaba84180b1b259033d0f2ec4
Daniel Samson <12231216+daniel-samson@users.noreply.github.com>
2026-01-21 23:43:23 +0000

    chore: update to v0.1.1

--- Formula/barn.rb
+++ Formula/barn.rb
@@ -1,13 +1,13 @@
 class Barn < Formula
   desc "Cross-platform job daemon for media processing"
   homepage "https://github.com/samson-media/barn"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-macos-arm64"
-      sha256 "96465d84451f6b2a338378dd8f6c68ed5998544d7f2527cca7e868c531d10c7f"
+      sha256 "8566536b93acb2337233b9a87870ecd95a141c4fc41fe94b90a6f941eefab506"
 
       def install
         bin.install "barn-macos-arm64" => "barn"
@@ -18,7 +18,7 @@ class Barn < Formula
   on_linux do
     on_arm do
       url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-linux-arm64"
-      sha256 "f3f1dea009d242847e128b086bc87c423e02be6f734b874f7513b9f221a88aeb"
+      sha256 "7c82b8de0d89ef08a8f5801a67f8d4e646836c8bc3ecc47a4655f00de341af8e"
 
       def install
         bin.install "barn-linux-arm64" => "barn"
@@ -27,7 +27,7 @@ class Barn < Formula
 
     on_intel do
       url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-linux-x64"
-      sha256 "45107eb15f676a749ebbdd350587d4167ae9756c0755dc9bff7d5e6059627af1"
+      sha256 "7ca3cdf7a0977887dbdd48847e513389eb01d44604f75e7d7817cf12b72d13fa"
 
       def install
         bin.install "barn-linux-x64" => "barn"
