c0831efbacf793c572da267ad05e81cbb7c63e00
Daniel Samson <12231216+daniel-samson@users.noreply.github.com>
2026-01-21 23:23:26 +0000

    fix: update checksums for v0.1.0

--- Formula/barn.rb
+++ Formula/barn.rb
@@ -7,39 +7,32 @@ class Barn < Formula
   on_macos do
     on_arm do
       url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-macos-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "96465d84451f6b2a338378dd8f6c68ed5998544d7f2527cca7e868c531d10c7f"
 
       def install
         bin.install "barn-macos-arm64" => "barn"
       end
     end
-
-    on_intel do
-      url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-macos-x64"
-      sha256 "PLACEHOLDER"
-
-      def install
-        bin.install "barn-macos-x64" => "barn"
-      end
-    end
   end
 
   on_linux do
+    on_arm do
+      url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-linux-arm64"
+      sha256 "f3f1dea009d242847e128b086bc87c423e02be6f734b874f7513b9f221a88aeb"
+
+      def install
+        bin.install "barn-linux-arm64" => "barn"
+      end
+    end
+
     on_intel do
       url "https://github.com/samson-media/barn/releases/download/v#{version}/barn-linux-x64"
-      sha256 "PLACEHOLDER"
+      sha256 "45107eb15f676a749ebbdd350587d4167ae9756c0755dc9bff7d5e6059627af1"
 
       def install
         bin.install "barn-linux-x64" => "barn"
       end
     end
-  end
-
-  service do
-    run [opt_bin/"barn", "service", "run"]
-    keep_alive true
-    log_path var/"log/barn.log"
-    error_log_path var/"log/barn.error.log"
   end
 
   test do
